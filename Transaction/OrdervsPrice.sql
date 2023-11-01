ec598d099f8417ad2665ef7e71c6acf784fcde65
NamJoi <namndnsws00154@fpt.edu.vn>
2023-11-01 10:11:13 +0700

    Update Transaction

--- Transaction/OrdervsPrice.sql
+++ Transaction/OrdervsPrice.sql
@@ -1,31 +1,42 @@
 START TRANSACTION;
+--Insert values into Users table
 INSERT INTO Users (Username, Password, Email, UserType)
 VALUES ('Name A', 'PasswordB', 'userb@example.com', 'Customer');
+--Select UserID
 SELECT @UserID := LAST_INSERT_ID();
+--Insert values into Status table
 INSERT INTO Status (Description) VALUES ('1');
 INSERT INTO Status (Description) VALUES ('2');
+--Select StatusID
 SELECT @StatusID1 := LAST_INSERT_ID();
 SELECT @StatusID2 := LAST_INSERT_ID();
+--Insert values into Businesses table
 INSERT INTO Businesses (BusinessName, OwnerName, Address, ContactNumber, UserID)
 VALUES ('BusinessB', 'OwnerB', 'BusinessAddress1', '1234567890', @UserID);
+--Select BusinessID
 SELECT @BusinessID1 := LAST_INSERT_ID();
+--Insert values into Products table
 INSERT INTO Products (ProductName, Description, Price, Type, StockQuantity, BusinessID)
 VALUES ('Product A', 'Describe product A', 50.00, 'Type A', 100, @BusinessID1),
        ('Product B', 'Describe product B', 60.00, 'Type B', 150, @BusinessID1),
        ('Product C', 'Describe product C', 70.00, 'Type A', 200, @BusinessID1);
+--Select ProductID 
 SELECT @ProductID1 := LAST_INSERT_ID();
 SELECT @ProductID2 := LAST_INSERT_ID() - 1;
 SELECT @ProductID3 := LAST_INSERT_ID() - 2;
+--Insert values into Orders table
 INSERT INTO Orders (OrderDate, TotalPrice, StatusID, UserID)
 VALUES (NOW(), 0, 1, @UserID);
 SET @OrderID := LAST_INSERT_ID();
 SET @Quantity1 = 1;
 SET @Quantity2 = 2;
 SET @Quantity3 = 3;
+--Insert values into OrderItems table
 INSERT INTO OrderItems (OrderID, ProductID, Quantity, Price, Subtotal)
 VALUES (@OrderID, @ProductID1, @Quantity1, 50.00, 150.00),
        (@OrderID, @ProductID2, @Quantity2, 60.00, 120.00),
        (@OrderID, @ProductID3, @Quantity3, 70.00, 210.00);
+--Update TotalPrice of Orders table by sum of Subtotal in OrderItems table which have 'OrderID = @OrderID' condition
 UPDATE Orders
 SET TotalPrice = (SELECT SUM(Subtotal) FROM OrderItems WHERE OrderID = @OrderID)
 WHERE OrderID = @OrderID;
--- Transaction/Payments.sql
+++ Transaction/Payments.sql
@@ -1,20 +1,20 @@
 START TRANSACTION;
 SELECT @OrderID := OrderID FROM Orders ORDER BY OrderID DESC LIMIT 1;
-
 --Calculate the total product added
 SELECT COUNT(*) INTO @NumberOfProduct FROM Products;
+--Insert values into Invoices table
 INSERT INTO Invoices (OrderID, InvoiceDate, Amount, BillingAddress, StatusID)
 VALUES (@OrderID, NOW(), @NumberOfProduct, '123 Street, City, Country', 1);
 SELECT @TotalPrice := TotalPrice FROM Orders WHERE OrderID = @OrderID;
 INSERT INTO Payments (PaymentDate, Amount, PaymentMethod, OrderID)
 VALUES (NOW(), @TotalPrice, 'Payment Method', @OrderID);
-
 --Set Status
 UPDATE Orders SET StatusID = 2 WHERE OrderID = @OrderID;
 SELECT @StatusID := StatusID FROM Status ORDER BY StatusID DESC LIMIT 1;
 SELECT @UserID := UserID FROM  Users ORDER BY  UserID DESC LIMIT 1;
-
 -- Send notifications to users
 INSERT INTO Notifications (Title, Message, SendDate, UserID, StatusID, Type)
 VALUES ('Payment Notifications', 'Payment Successful', NOW(), @UserID, @StatusID, 'Clothes');
+--Delete values of Products table after Payments
+DELETE FROM Products WHERE OrderID = @OrderID;
 COMMIT;
